d135b0c2a150a397d773ca55f925150a72a80499
Linen220 <fernandorondon2009@hotmail.com>
2023-06-01 01:20:42 -0500

    add style

--- js/main.js
+++ js/main.js
@@ -4,5 +4,25 @@ const bgColor= document.querySelector('.bg-color');
 
 title.addEventListener('click', ()=> {
     bgColor.classList.add('active');
-    console.log('hola');
+    // Realza un tiempo fuera, un break para que ejecute
+    // una accicón, osea ejecutar lo que esta dentro de la
+    // función anonima
+    // Recibe 2 parametros: una función y un numero
+    // que representa el tiempo en milisegundos
+    setTimeout(function() {
+        bgColor.classList.remove('active');
+    }, 1200);
+
+    const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F","G","H"];
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
+
+    bgColor.style.background = hexColor;
+    curved.style.background = hexColor;
+
+    function getRandomNumber() {
+        return Math.floor(Math.random()*hex.length);   
+    }
 });
